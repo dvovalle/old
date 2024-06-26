7287ec99a19d930f376646581bf2cc670cd6e08f
Danilo Valle de Oliveira <daniloka@kalunga.com.br>
2024-06-26 12:50:04 -0300

    ⬆️ Update Lista Nova
    Atualizada

---
+++ cleanstr.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for x in $(ls M3UListas/) 
+do
+    echo $x
+    sed -i '' '/#EXTM3U/d' M3UListas/$x
+    sed -i '' '/##/d' M3UListas/$x
+    sed -i '' '/^[[:space:]]*$/d' M3UListas/$x
+done
