24a649cb1dec6dee591c4e3b2ffd09134d4d0a24
Danilo Valle de Oliveira <daniloka@kalunga.com.br>
2025-08-26 18:58:08 -0300

    🔖 Lista de 2025-08-26
    Nova lista 2025-08-26

--- main.py
+++ main.py
@@ -424,11 +424,7 @@ def __analise(grupo: str = "*", verify: bool = True) -> bool:
 
 
 def __start_analise(verify: bool = True) -> None:
-    list_gr: list[str] = [
-"FILMES | DESENHOS",
-"FILMES | HARY POTER",
-"FILMES | PIRATAS DO CARIBE"]
-#"SERIES | OUTLANDER"]
+    list_gr: list[str] = ["FILMES | MARVEL DC"]
 
     if list_gr is not None and len(list_gr) > 0:
         if len(list_gr) > 1:
