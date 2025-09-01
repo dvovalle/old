5f3e0ad6e1e38ea9ae3abc01d2cabbcbf66fe3e0
Danilo Valle de Oliveira <daniloka@kalunga.com.br>
2025-09-01 18:23:29 -0300

    🔖 Lista de 2025-09-01 as 18:23h
    Nova lista de 2025-09-01

--- main.py
+++ main.py
@@ -424,7 +424,18 @@ def __analise(grupo: str = "*", verify: bool = True) -> bool:
 
 
 def __start_analise(verify: bool = True) -> None:
-    list_gr: list[str] = ["SERIES | SUPERMAN E LOIS"]
+    list_gr: list[str] = [
+"FILMES | HARY POTER",
+"FILMES | KARATE KID",
+"FILMES | RAMBO",
+"FILMES | ROCKY UM LUTADOR",
+"SERIES | CHEFE DE GUERRA",
+"SERIES | COBRA KAI",
+"SERIES | OUTLANDER",
+"SERIES | PACIFICADOR",
+"SERIES | RUPTURA",
+"SERIES | SILO",
+"SERIES | SWAGGER",]
 
     if list_gr is not None and len(list_gr) > 0:
         if len(list_gr) > 1:
